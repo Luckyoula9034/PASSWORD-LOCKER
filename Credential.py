0fc217ca1ae2fc1672ae2f4d7067f4563588c8f3
luqqy <you@example.com>
2020-09-04 11:31:13 +0300

    added credentials_test.py file

---
+++ Credential.py
@@ -0,0 +1,16 @@
+class Credential:
+  """
+  class that generates new instances of Credential
+  """
+  pass
+
+Credential_list =["Lucky","Oula","0759099034","luckyoula@gmail.com"] #empty credential list
+
+def __init__(self,credential_name,user_name,password,email):
+
+# docstring removed for simpilicity
+  
+  self.credential_name = credential_name
+  self.user_name  = user_name
+  self.password = password
+  self.email      = email
