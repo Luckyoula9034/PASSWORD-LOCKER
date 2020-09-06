bc510bc343798e058438fbbf2d27b5a6dd583058
luqqy <you@example.com>
2020-09-06 15:23:53 +0300

    added test_find_user_by_name test method to check if we can find a user by user name and display information

--- user_test.py
+++ user_test.py
@@ -61,3 +61,16 @@ def test_delete_user(self):
 
         self.new_user.delete_user()  # Deleting a user object
         self.assertEqual(len(User.user_list), 1)
+def test_find_user_by_name(self):
+        '''
+        test to check if we can find a user by user name and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User(
+            "Test", "lucky", "0759099034", "luckyoula@gmail.com")  # new user
+        test_user.save_user()
+
+        found_user = User.find_by_name("lucky")
+
+        self.assertEqual(found_user.email, test_user.email)
