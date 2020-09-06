79f36cc8ec3d7096db059298498309369937fe8f
luqqy <you@example.com>
2020-09-06 15:53:49 +0300

    added save_users method to save users detail

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ def create_user(f_name, l_name, password, email):
     '''
     new_user = User(f_name, l_name, password, email)
     return new_user
+    
+
+def save_users(user):
+    '''
+    Function to save user detail
+    '''
+    user.save_user()
