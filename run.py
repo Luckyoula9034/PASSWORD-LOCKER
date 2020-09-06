81d6fcfeb539384c08daa9ff8a79b7710eca6260
luqqy <you@example.com>
2020-09-06 15:51:55 +0300

    added create_user method to create a new user

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3.6
 from user import User
 from credential import Credential
+
+def create_user(f_name, l_name, password, email):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(f_name, l_name, password, email)
+    return new_user
