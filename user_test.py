6acd46e564fed43b875f795ccc580eee6639f4ad
luqqy <you@example.com>
2020-09-06 14:49:38 +0300

    added a testUser method to test cases for credential class behavior

--- user_test.py
+++ user_test.py
@@ -1,2 +1,12 @@
 import unittest  # Importing the unittest module
 from user import User  # Importing the credential class
+
+class TestUser(unittest.TestCase):
+     '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    
+    '''
+    
