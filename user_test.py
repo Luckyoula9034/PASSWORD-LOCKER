904490e7440b7c90022836eea6d413c58699f667
luqqy <you@example.com>
2020-09-06 15:07:44 +0300

    test_save_user test case to test if the credential object is saved into the user list

--- user_test.py
+++ user_test.py
@@ -18,11 +18,18 @@ def setUp(self):
             "lucky", "oula", "0759099034", "luckyoula@gmail.com")  # create credential object
 
 def test_init(self):
-    
+
     '''
     test_init test case to test if the object is initialized properly
     '''
     self.assertEqual(self.new_user.account_name, "lucky")
     self.assertEqual(self.new_user.user_name, "oula")
     self.assertEqual(self.new_user.password, "0759099034")
-    self.assertEqual(self.new_user.email, "luckyoula@gmail.com")            
+    self.assertEqual(self.new_user.email, "luckyoula@gmail.com")
+def test_save_user(self):
+        '''
+        test_save_user test case to test if the credential object is saved into
+         the user list
+        '''
+        self.new_user.save_user()  # saving the new user
+        self.assertEqual(len(User.user_list), 1)
