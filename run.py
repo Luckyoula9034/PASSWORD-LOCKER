e1ad73304909f5ec37221b476be2d6005977da13
luqqy <you@example.com>
2020-09-06 16:12:29 +0300

    added while funtionality to make sure the details entered are printed

--- run.py
+++ run.py
@@ -51,3 +51,37 @@ def main():
 
     print(f"Hello.{user_name} please follow the procedures carefully to create account?")
     print('\n')
+
+    while True:
+        print("Use these short codes : cc - create a new user, dc - display users, fc -find a user, ex -exit the account,I ")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print("New User")
+            print("-"*100)
+
+            print("First name ....")
+            f_name = input('First Name:')
+            print('\n')
+
+            print("Last name ...")
+            l_name = input('Last Name:')
+            print('\n')
+
+            print("Password ...:")
+            password = input('Password:')
+            print('\n')
+
+            print("Email address ...")
+            email = input('Email:')
+            print('\n')
+
+            # create and save new user.
+            save_users(create_user(
+                f_name, l_name, password, email))
+            print('\n')
+            print(f"New User {f_name} {l_name} created")
+            print(
+                f"You can now login to your {f_name} account using your password.")
+            print('\n')
