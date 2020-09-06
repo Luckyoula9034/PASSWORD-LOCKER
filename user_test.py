7300893f5cb2f0d6e550a8550f6a1f18ed03c67a
luqqy <you@example.com>
2020-09-06 15:34:56 +0300

    added test_user_exist to test if we can return a Boolean  if we cannot find the user.

--- user_test.py
+++ user_test.py
@@ -74,3 +74,18 @@ def test_find_user_by_name(self):
         found_user = User.find_by_name("lucky")
 
         self.assertEqual(found_user.email, test_user.email)
+
+
+def test_user_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the user.
+        '''
+
+        self.new_user.save_user()
+        test_user = User(
+            "Test", "lucky", "0759099034", "luckyoula@gmail.com")  # new user
+        test_user.save_user()
+
+        user_exists = User.user_exist("0759099034")
+
+        self.assertTrue(user_exists)
