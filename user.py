2a06394eb00861acc4a76468b9c6de0a5b710c7f
luqqy <you@example.com>
2020-09-06 12:02:03 +0300

    added a self method to represents the instance of the object itself

--- user.py
+++ user.py
@@ -5,4 +5,9 @@ class User:
     user_list = [] #empty credential list
 
     def __init__(self, account_name, user_name, password, email):
-        
+
+       
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email 
